b3c5b99ff580100145b966a9e787fa081f687da8
alex <alex.shonia123@gmail.com>
2024-03-21 21:54:43 +0400

    improve form widget with abstraction & add validation to contact page

--- views/contact.php
+++ views/contact.php
@@ -1,22 +1,16 @@
 <?php
+use app\core\form\TextareaField;
 
 /** @var  $this \app\core\View */
+/** @var  $model \app\models\ContactForm */
 
 $this->title = 'Contact';
 ?>
 <h1>Contact Us</h1>
-<form action="" method="post">
-    <div class="mb-3">
-        <label class="form-label">Subject</label>
-        <input type="text" name="subject" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label class="form-label">Email</label>
-        <input type="email" name="email" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label class="form-label">Body</label>
-        <textarea name="body" class="form-control"></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+
+<?php $form = \app\core\form\Form::begin('', 'post') ?>
+<?php echo $form->field($model, 'subject') ?>
+<?php echo $form->field($model, 'email') ?>
+<?php echo new TextareaField($model, 'body') ?>
+<button type="submit" class="btn btn-primary">Submit</button>
+<?php \app\core\form\Form::end() ?>
